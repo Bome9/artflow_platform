e7589d40046d708f5f396083ea1b8e515cad40a8
Zakhar Boldinov <developdr3am@gmail.com>
2023-10-08 18:28:17 +0300

    creating main app

--- artflow/artflow/settings.py
+++ artflow/artflow/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*nvavc8(5waq7y-o4j5-vzr))ut4c)p6g2ave+2s@ioqn1sl78'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
