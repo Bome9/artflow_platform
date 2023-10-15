40e779e1685e7e77e9d47a33481f3ecadaae4c5c
Zakhar Boldinov <developdr3am@gmail.com>
2023-10-15 14:13:45 +0300

    creating main app

--- artflow/main/views.py
+++ artflow/main/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('ПРИВЕТ МИР!')
+    return render(request, 'main/index.html')
