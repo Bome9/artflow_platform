3aaed54d3bfc45539a084f04074e454b06435a9e
Zakhar Boldinov <developdr3am@gmail.com>
2023-11-06 18:36:15 +0300

    creating main app

--- artflow/artflow/urls.py
+++ artflow/artflow/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('accounts.urls')),
-    path('', include('publications.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
